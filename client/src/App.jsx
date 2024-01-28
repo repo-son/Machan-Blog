54eb030b36661c78955885041baca04ca974cd2e
repo-son <lahiru980529@gmail.com>
2024-01-28 17:49:43 +0530

    Created responsive header components using flowbite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,10 +7,12 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
+import Header from "./Components/Header"
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
