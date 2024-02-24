fc09b6bad6c25efbba0ac4b67f5138df92f7fe83
repo-son <lahiru980529@gmail.com>
2024-02-25 02:09:52 +0300

    Show users to the admin dashboard

--- client/src/Components/DashPosts.jsx
+++ client/src/Components/DashPosts.jsx
@@ -87,8 +87,7 @@ export default function DashPosts() {
               </Table.HeadCell>
             </Table.Head>
             {userPosts.map((post) => (
-              // eslint-disable-next-line react/jsx-key
-              <Table.Body className='divide-y'>
+              <Table.Body className='divide-y' key={post._id}>
                 <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
                   <Table.Cell>
                     {new Date(post.updatedAt).toLocaleDateString()}
