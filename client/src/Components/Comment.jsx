d76662c2cc59e14b864273ab829a89340b0e865f
repo-son <lahiru980529@gmail.com>
2024-02-27 20:31:19 +0300

    Add delete functionality to the comment section

--- client/src/Components/Comment.jsx
+++ client/src/Components/Comment.jsx
@@ -4,7 +4,7 @@ import { FaThumbsUp } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { Button, Textarea } from "flowbite-react";
 
-export default function Comment({comment, onLike, onEdit}) {
+export default function Comment({comment, onLike, onEdit, onDelete}) {
   const [user, setUser] = useState({});
   const {currentUser} = useSelector((state) => state.user);
   const [isEditing, setIsEditing] = useState(false);
@@ -100,13 +100,22 @@ export default function Comment({comment, onLike, onEdit}) {
           </p>
           {
             currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
+              <>
               <button
-              type='button'
-              onClick={handleEdit}
-              className='text-gray-400 hover:text-blue-500'
+                type='button'
+                onClick={handleEdit}
+                className='text-gray-400 hover:text-blue-500'
               >
                 Edit
               </button>
+              <button
+                type='button'
+                onClick={() => onDelete(comment._id)}
+                className='text-gray-400 hover:text-red-500'
+              >
+                Delete
+              </button>
+              </>
             )
           }
         </div>
--- client/src/Components/CommentSection.jsx
+++ client/src/Components/CommentSection.jsx
@@ -1,8 +1,9 @@
-import { Alert, Button, Textarea } from 'flowbite-react'
+import { Alert, Button, Modal, Textarea, ModalHeader } from 'flowbite-react'
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
 import Comment from './Comment'
+import {HiOutlineExclamationCircle} from 'react-icons/hi'
 
 export default function CommentSection({postId}) {
   const { currentUser } = useSelector(state => state.user)
@@ -10,6 +11,8 @@ export default function CommentSection({postId}) {
   const [commentError, setCommentError] = useState(null);
   const [comments, setComments] = useState([]);
   const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
+  const [commentToDelete, setCommentToDelete] = useState(null);
   const handleSubmit = async (e) => {
     e.preventDefault();
     if(comment.length > 200){
@@ -82,6 +85,25 @@ export default function CommentSection({postId}) {
       )
     );
   };
+
+  const handleDelete = async (commentId) => {
+    setShowModal(false);
+    try {
+      if(!currentUser){
+        navigate('/sign-in');
+        return;
+      }
+      const res = await fetch(`/api/comment/deleteComment/${commentId}`,{
+        method: 'DELETE',
+      });
+      if(res.ok){
+        const data = await res.json();
+        setComments(comments.filter((comment) => comment._id !== commentId));
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
       {currentUser ?
@@ -140,11 +162,30 @@ export default function CommentSection({postId}) {
           comment={comment}
           onLike={handleLike}
           onEdit={handleEdit}
+          onDelete={(commentId) =>{
+            setShowModal(true)
+            setCommentToDelete(commentId)
+          }}
           />
         ))
       }
     </>
     )}
+    <Modal show={showModal} onClose={() => setShowModal(false)} popup size='md'>
+        <ModalHeader/>
+          <Modal.Body>
+            <div className="text-center">
+              <HiOutlineExclamationCircle className='h-14 w-14 text-red-500 dark:text-red-400 mb-4 mx-auto' />
+              <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+                Are you sure you want to Delete this comment?
+              </h3>
+              <div className='flex justify-center gap-4'>
+                <Button color='failure' onClick={() => handleDelete(commentToDelete)}>Delete</Button>
+                <Button onClick={() => setShowModal(false)}>Cancel</Button>
+              </div>
+            </div>
+          </Modal.Body>
+      </Modal>
     </div>
   )
 }
