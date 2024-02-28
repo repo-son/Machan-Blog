d9720f8e7f956a028d91eb05e49cad0992b90b7a
repo-son <lahiru980529@gmail.com>
2024-02-29 01:11:08 +0300

    Complete search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import CreatePost from "./pages/CreatePost"
 import UpdatePost from "./pages/UpdatePost"
 import PostPage from "./pages/PostPage"
 import ScrollToTop from "./Components/ScrollToTop"
+import Search from "./pages/Search"
 
 export default function App() {
   return (
@@ -26,6 +27,7 @@ export default function App() {
       <Route path="/about" element={<About />} />
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/search" element={<Search />} />
       <Route element={<PrivateRoute/>}>
        <Route path="/dashboard" element={<Dashboard />} />
       </Route>
