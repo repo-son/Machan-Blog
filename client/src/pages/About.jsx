90a4bb3fdd0a7a5a292bfe176b16bddf39ee1eec
repo-son <lahiru980529@gmail.com>
2024-02-29 10:11:23 +0300

    Complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,17 @@
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>About Machan Blog</h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>This blog was created because I love to write articles and post them. So here is the collection of best articles that I wrote. So enjoy reading them and do not forget to add your thoughts and place a like for me. Thank you guys...</p>
+            <p>From bustling metropolises to remote villages, from pristine beaches to rugged mountain peaks, our blog is a virtual compass guiding you through the wonders of our planet. Whether you are a seasoned globetrotter or an armchair traveler dreaming of distant shores, Wanderlust Chronicles invites you to embark on a journey of discovery, one destination at a time.</p>
+            <p>Through vivid storytelling, captivating photography, and practical advice, we aim to inspire and empower you to explore the world with an open heart and curious mind. From off-the-beaten-path adventures to luxury getaways, we cover a spectrum of travel experiences, catering to every wanderers desires and aspirations.</p>
+            <p>So pack your bags, buckle up, and get ready for the adventure of a lifetime. The world is waiting to be explored, and Wanderlust Chronicles is your companion on this epic journey of discovery.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
